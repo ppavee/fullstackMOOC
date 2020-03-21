ba61571df2ac3a10ffd93a5092e907df80a1e742
ppavee <paavorantala94@gmail.com>
2020-03-21 10:27:04 +0200

    unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,13 +2,36 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Title = ({text}) => <h1>{text}</h1>
+const Title = ({ text }) => <h1>{text}</h1>
 
 
-const Button = ({text, handleClick}) => <button onClick={handleClick} >{text}</button>
+const Button = ({ text, handleClick }) => <button onClick={handleClick} >{text}</button>
 
 
-const Stats = ({text, value}) => <div>{text} {value}</div>
+const Statistics = ({ good, neutral, bad }) => {
+  let feedbackAmount = good + neutral + bad
+
+  return (
+    <div>
+      <Title text='statistics' />
+      <Stats text='good' value={good} />
+      <Stats text='neutral' value={neutral} />
+      <Stats text='bad' value={bad} />
+      <Stats text='all' value={feedbackAmount} />
+      <Stats text='average' value={
+        (good * 1 + neutral * 0 + bad * -1) / feedbackAmount || 0
+      }
+      />
+      <Stats text='positive' value={
+        ((good / feedbackAmount) * 100 || 0) + ' %'
+      }
+      />
+    </div>
+  )
+}
+
+
+const Stats = ({ text, value }) => <div>{text} {value}</div>
 
 
 const App = () => {
@@ -21,31 +44,17 @@ const App = () => {
   const incrementNeutral = () => setNeutral(neutral + 1)
   const incrementBad = () => setBad(bad + 1)
 
-  let feedbackAmount = good + neutral + bad
-
   return (
     <div>
       <Title text='give feedback' />
       <Button text='good' handleClick={incrementGood} />
       <Button text='neutral' handleClick={incrementNeutral} />
       <Button text='bad' handleClick={incrementBad} />
-      <Title text='statistics' />
-      <Stats text='good' value={good} />
-      <Stats text='neutral' value={neutral} />
-      <Stats text='bad' value={bad} />
-      <Stats text='all' value={feedbackAmount} />
-      <Stats text='average' value={
-          (good*1 + neutral*0 + bad*-1) / feedbackAmount || 0
-          } 
-      />
-      <Stats text='positive' value={
-          ((good/feedbackAmount)*100 || 0) +' %'
-          } 
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
