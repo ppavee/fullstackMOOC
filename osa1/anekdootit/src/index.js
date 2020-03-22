da8a60a481cdf46f5938a28aeffb23172eb48ec6
ppavee <paavorantala94@gmail.com>
2020-03-22 10:46:10 +0200

    anekdootit step3

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -5,29 +5,50 @@ import ReactDOM from 'react-dom'
 const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
 
 
-const Votes = ({ votes }) => <p>has {votes} votes</p>
+const Votes = ({ votes }) => <div>has {votes} votes</div>
+
+
+const Title = ({text}) => <h1>{text}</h1>
+
+
+const Anecdote = ({text, voteAmount}) => {
+  return (
+    <div>
+      <div>{text}</div>
+      <Votes votes={voteAmount} />
+    </div>
+  )
+}
+
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(
-    Array.apply(null, new Array(anecdotes.length))
+    Array.apply(null, new Array(props.anecdotes.length))
       .map(Number.prototype.valueOf, 0)
   )
+  const [mostVoted, setMostVoted] = useState(0)
 
-  const nextAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const nextAnecdote = () => setSelected(Math.floor(Math.random() * props.anecdotes.length))
 
   const voteAnecdote = () => {
     const votesCopy = [...votes]
     votesCopy[selected] += 1
+    if(votesCopy[selected] > votes[mostVoted]) {
+      setMostVoted(selected)
+    }
     setVotes(votesCopy)
   }
 
+
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <Votes votes={votes[selected]} />
+      <Title text='Anecdote of the day' />
+      <Anecdote text={props.anecdotes[selected]} voteAmount={votes[selected]} />
       <Button text='vote' handleClick={voteAnecdote} />
       <Button text='next anecdote' handleClick={nextAnecdote} />
+      <Title text='Anecdote with most votes' />
+      <Anecdote text={props.anecdotes[mostVoted]} voteAmount={votes[mostVoted]} />
     </div>
   )
 }
