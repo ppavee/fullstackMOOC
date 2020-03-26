f582b9c1aa4b01981df9573938068ca650708a65
Paavo Rantala <paavo.rantala@kosila.fi>
2020-03-26 08:22:59 +0200

    kurssitiedot step6

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({name}) => {
+    return (
+        <h1>
+            {name}
+        </h1>
+    )
+}
+
+const Content = ({parts}) => {
+    return (
+        <div>
+            {parts.map(part => 
+                <Part key={part.id} part={part} />
+            )}
+        </div>
+    )
+}
+
+const Part = ({part}) => {
+    return (
+        <p>
+            {part.name} {part.exercises}
+        </p>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} /> 
+        </div>
+    )
+}
+
+const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Course course={course} />
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'))
