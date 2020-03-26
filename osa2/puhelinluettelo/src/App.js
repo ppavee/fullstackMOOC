2edc5820bdcf5256b61646e7943749189694695d
ppavee <paavorantala94@gmail.com>
2020-03-26 21:38:03 +0200

    puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1231244' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [searchTerm, setNewSearchTerm] = useState('')
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
@@ -13,6 +17,10 @@ const App = () => {
 
     const handleNumberChange = (event) => {
         setNewNumber(event.target.value)
+    }
+
+    const handleSearchTermChange = (event) => {
+        setNewSearchTerm(event.target.value)
     }
 
     const addPerson = (event) => {
@@ -30,9 +38,17 @@ const App = () => {
             setNewNumber('')
         }
     }
+
+    const filteredPersons = persons.filter(person => (!searchTerm || person.name.toLowerCase().includes(searchTerm.toLowerCase())))
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with
+                <input value={searchTerm} onChange={handleSearchTermChange} />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={addPerson} >
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
@@ -45,7 +61,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person =>
+            {filteredPersons.map(person =>
                 <p key={person.name}>
                     {person.name} {person.number}
                 </p>
