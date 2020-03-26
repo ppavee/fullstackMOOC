ee462b1b9c25d0ebb151c852e97769af5b17c80d
Paavo Rantala <paavo.rantala@kosila.fi>
2020-03-26 12:48:07 +0200

    kurssitiedot step7

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -10,16 +10,24 @@ const Header = ({name}) => {
 }
 
 const Content = ({parts}) => {
+    const totalExercises = parts
+        .map(part => part.exercises)
+        .reduce((sum, value) => {
+            return sum + value
+        }, 0)
     return (
         <div>
-            {parts.map(part => 
+            {parts.map(part =>
                 <Part key={part.id} part={part} />
             )}
+            <p>
+                <b>total of {totalExercises} exercises</b>
+            </p>
         </div>
     )
 }
 
-const Part = ({part}) => {
+const Part = ({ part }) => {
     return (
         <p>
             {part.name} {part.exercises}
@@ -38,24 +46,30 @@ const Course = ({course}) => {
 
 const App = () => {
     const course = {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        }
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 4
+            },
+
       ]
     }
   
