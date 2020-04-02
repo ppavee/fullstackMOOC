8b7c9a383faea405ce2989f260ec96d868968bf3
Paavo Rantala <paavo.rantala@kosila.fi>
2020-04-02 08:01:27 +0300

    tietokantaa käyttävä versio internettiin

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -41,7 +41,7 @@ const App = () => {
         event.preventDefault()
         const names = persons.map(person => person.name)
         const indexOfPerson = names.indexOf(newName)
-        if (indexOfPerson !== -1 && newNumber) {
+        if (newNumber && indexOfPerson !== -1) {
             if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
                 const oldPerson = persons[indexOfPerson]
                 const newPerson = {...oldPerson, number: newNumber}
@@ -71,6 +71,10 @@ const App = () => {
                     setNewNumber('')
                     setSuccessMessage(`Added ${newPerson.name}`)
                     setTimeout(() => setSuccessMessage(null), 5000)
+                })
+                .catch(error => {
+                    setErrorMessage(error.response.data.error)
+                    setTimeout(() => setErrorMessage(null), 5000)
                 })
 
         } else {
